e6929425a4f5e166ef91242fb46e3e5ad3639fe3
dm13y_work <Admin@mercom.by>
2017-08-23 14:34:15 +0300

    added doc for the constructor

--- src/main/java/by/mercom/dev/scales/shtrih5000/cmd/CmdLockKeyboard.java
+++ src/main/java/by/mercom/dev/scales/shtrih5000/cmd/CmdLockKeyboard.java
@@ -5,6 +5,11 @@ import by.mercom.dev.scales.shtrih5000.cmd.core.ScaleCommand;
 
 public class CmdLockKeyboard extends ScaleCommand {
 
+    /**
+     * Блокировка / разблокировка клавиатуры
+     * @param pwd Пароль администратора
+     * @param keyLock Признак ключения/отключения блокировки
+     */
     public CmdLockKeyboard(Param.Password pwd, Param.KeyLock keyLock) {
         super(0x09, 6, new Param[]{pwd, keyLock}, true);
         setDescription("Блокировка / разблокировка клавиатуры");
